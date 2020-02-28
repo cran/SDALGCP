69ffd810619b5bc706a4a297774bda5e2f271691
Olatunji Johnson <olatunjijohnson21111@gmail.com>
2020-02-28 15:40:02 +0000

    version 0.3.0

--- inst/doc/SDALGCP-vignette.R
+++ inst/doc/SDALGCP-vignette.R
@@ -1,48 +1,48 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
   eval= FALSE
 )
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #  require(SDALGCP)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #  data("PBCshp")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #  data <- as.data.frame(PBCshp@data)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #  data("pop_den")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #  pop_den[is.na(pop_den[])] <- 0
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #  FORM <- X ~ propmale + Income + Employment + Education + Barriers + Crime +
 #    Environment +  offset(log(pop))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #  phi <- seq(500, 1700, length.out = 20)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  my_est <- SDALGCPMCML(data=data, formula=FORM, my_shp=PBCshp, delta=200, phi=phi, method=1, pop_shp=pop_den,
 #                        weighted=TRUE, par0=NULL, control.mcmc=NULL)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  summary(my_est)
 #  #and for confidence interval use
 #  confint(my_est)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  phiCI(my_est, coverage = 0.95, plot = TRUE)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  Dis_pred <- SDALGCPPred(para_est=my_est,  continuous=FALSE)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  #to map the incidence
 #  plot(Dis_pred, type="incidence", continuous = FALSE)
 #  #and its standard error
@@ -54,10 +54,10 @@ knitr::opts_chunk$set(
 #  #to map the exceedance probability that the covariate-adjusted relative risk is greter than a particular threshold
 #  plot(Dis_pred, type="CovAdjRelRisk", continuous = FALSE, thresholds=3.0)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  Con_pred <- SDALGCPPred(para_est=my_est, cellsize = 300, continuous=TRUE)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  #to map the covariate adjusted relative risk
 #  plot(Con_pred, type="relrisk")
 #  #and its standard error
@@ -65,11 +65,11 @@ knitr::opts_chunk$set(
 #  #to map the exceedance probability that the relative risk is greter than a particular threshold
 #  plot(Con_pred, type="relrisk", thresholds=1.5)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  my_est <- SDALGCPMCML(data=data, formula=FORM, my_shp=PBCshp, delta=200, phi=phi, method=1,
 #                        weighted=FALSE,  plot=FALSE, par0=NULL, control.mcmc=NULL, messages = TRUE, plot_profile = TRUE)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  require(rgdal)
 #  require(sp)
 #  require(spacetime)
@@ -79,7 +79,7 @@ knitr::opts_chunk$set(
 #  ohio_shp <- rgdal::readOGR("ohio_shapefile/","tl_2010_39_county00")
 #  ohio_shp <- sp::spTransform(ohio_shp, sp::CRS("+init=epsg:32617"))
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  m <- length(ohio_shp)
 #  TT <- 21
 #  Y <- ohiorespMort$y
@@ -93,10 +93,10 @@ knitr::opts_chunk$set(
 #  time <- as.POSIXct(paste(1968:1988, "-01-01", sep = ""), tz = "")
 #  st_data <- spacetime::STFDF(sp = ohio_shp, time = time, data = data)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  spacetime::stplot(st_data[,,"Y"])
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  model.fit <- SDALGCPMCML_ST(formula=formula, st_data = st_data,  delta=800,
 #                              phi=phi, method=2, pop_shp=NULL,  kappa=0.5,
 #                              weighted=FALSE, par0=NULL, control.mcmc=control.mcmc,
@@ -104,16 +104,16 @@ knitr::opts_chunk$set(
 #                              giveup=50, messages=TRUE)
 #  summary(model.fit)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  dis_pred <- SDALGCPPred_ST(para_est = model.fit, continuous = FALSE)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  plot(dis_pred, type="CovAdjRelRisk", main="Relative Risk", continuous=FALSE)
 #  plot(dis_pred,  type="incidence", main="Incidence", continuous=FALSE)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  con_pred <- SDALGCPPred_ST(para_est = model.fit, cellsize = 2500, continuous=TRUE, n.window = 1)
 
-## ---- results = "hide",  warning = FALSE, message = FALSE----------------
+## ---- results = "hide",  warning = FALSE, message = FALSE---------------------
 #  plot(con_pred, type="relrisk", continuous=TRUE)
 
